3b2b460a25e3bb7438f528ebcd765dd9ba37378d
Lucas Batista <lucasdanbatista@edu.univali.br>
2024-11-18 20:24:25 -0300

    Add loading

--- lib/features/course/course_page.dart
+++ lib/features/course/course_page.dart
@@ -19,55 +19,62 @@ class CoursePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Matlingo'),
+        centerTitle: true,
       ),
-      drawer: const Drawer(),
       body: Observer(
-        builder: (context) => ListView(
-          padding: const EdgeInsets.all(24),
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.secondary,
-                borderRadius: const BorderRadius.all(Radius.circular(8)),
+        builder: (context) {
+          if (controller.course == null) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView(
+            padding: const EdgeInsets.all(24),
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.secondary,
+                  borderRadius: const BorderRadius.all(Radius.circular(8)),
+                ),
+                padding: const EdgeInsets.all(24),
+                child: Text(
+                  controller.course?.title ?? '',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium
+                      ?.copyWith(color: Colors.white),
+                ),
               ),
-              padding: const EdgeInsets.all(24),
-              child: Text(
-                controller.course?.title ?? '',
-                style: Theme.of(context)
-                    .textTheme
-                    .titleMedium
-                    ?.copyWith(color: Colors.white),
-              ),
-            ),
-            const SizedBox(height: 56),
-            ...controller.units.map(
-              (unit) {
-                const distance = 88.0;
-                final position = controller.units.indexOf(unit) + 1;
-                var padding = const EdgeInsets.only(right: distance);
-                if (position % 2 == 0) {
-                  padding = const EdgeInsets.only(left: distance);
-                } else if (position % 4 == 1) {
-                  padding = const EdgeInsets.only(right: distance);
-                } else if (position % 6 == 2) {
-                  padding = const EdgeInsets.only(left: distance * 3);
-                } else if (position % 8 == 3) {
-                  padding = const EdgeInsets.only(left: distance * 3);
-                }
-                return Center(
-                  child: Padding(
-                    padding: padding,
-                    child: UnitIcon(
-                      unit: unit,
-                      course: controller.course!,
-                      isUnlocked: true,
+              const SizedBox(height: 56),
+              ...controller.units.map(
+                (unit) {
+                  const distance = 88.0;
+                  final position = controller.units.indexOf(unit) + 1;
+                  var padding = const EdgeInsets.only(right: distance);
+                  if (position % 2 == 0) {
+                    padding = const EdgeInsets.only(left: distance);
+                  } else if (position % 4 == 1) {
+                    padding = const EdgeInsets.only(right: distance);
+                  } else if (position % 6 == 2) {
+                    padding = const EdgeInsets.only(left: distance * 3);
+                  } else if (position % 8 == 3) {
+                    padding = const EdgeInsets.only(left: distance * 3);
+                  }
+                  return Center(
+                    child: Padding(
+                      padding: padding,
+                      child: UnitIcon(
+                        unit: unit,
+                        course: controller.course!,
+                        isUnlocked: true,
+                      ),
                     ),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
+                  );
+                },
+              ),
+            ],
+          );
+        },
       ),
     );
   }
